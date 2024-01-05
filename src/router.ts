1e6f4dc2d964fef84ee11b9043fd320f24405ea9
Sergii <serhij.banha@tgadigital.com>
2024-01-05 11:03:10 +0200

    Add Slider Component Move Sign In form to separate component Combine Two Views in one Screen

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,6 @@ export const navigationRef = createNavigationContainerRef();
 
 export function navigate(name: NavigationTypes, params?: any) {
   if (navigationRef.isReady()) {
-    // @ts-ignore
     navigationRef.navigate(name, params);
   }
 }
