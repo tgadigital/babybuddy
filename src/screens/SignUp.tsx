8f38e9a797e14c0b0a2d3b48d64ace24e9060a24
Sergii <serhij.banha@tgadigital.com>
2024-01-05 13:09:09 +0200

    Restore changes

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,16 +1,15 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {FC} from 'react';
-import {Text, Button} from 'react-native';
+import {Text} from 'react-native';
 import {RootStackParamList} from '../types/navigation';
 import DefaultLayout from '../layouts/default';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
+type Props = NativeStackScreenProps<RootStackParamList, 'SignUp', 'Inside'>;
 
-const SignUp: FC<Props> = ({navigation}) => {
+const SignUp: FC<Props> = () => {
   return (
     <DefaultLayout>
       <Text>SignUp</Text>
-      <Button title={'Go back'} onPress={() => navigation.goBack()} />
     </DefaultLayout>
   );
 };
